c2a6a1de10318693e75b2c642a9487acb3aa7aa0
monojit18 <datta.monojit@gmail.com>
2020-03-27 10:28:04 +0530

    minor fix - comma removed

--- ValidateOCRApp-Setup/Setup/validateocrapp-create.ps1
+++ ValidateOCRApp-Setup/Setup/validateocrapp-create.ps1
@@ -2,7 +2,7 @@ param([Parameter(Mandatory=$false)] [string] $resourceGroup = "<resource_group>"
       [Parameter(Mandatory=$false)] [string] $subscriptionId = "<subscription_id>",
       [Parameter(Mandatory=$false)] [string] $baseFolderPath = "<folder_path>",
       [Parameter(Mandatory=$false)] [string] $keyVaultname = "<keyvault_name>",
-      [Parameter(Mandatory=$false)] [string] $objectId = "<object_Id>",)
+      [Parameter(Mandatory=$false)] [string] $objectId = "<object_Id>")
 
 $templatesFolderPath = $baseFolderPath + "/Templates"
 $keyvaultDeployCommand = "/keyvault-deploy.ps1 -rg $resourceGroup -fpath $templatesFolderPath -keyVaultName $keyVaultName -objectId $objectId"
