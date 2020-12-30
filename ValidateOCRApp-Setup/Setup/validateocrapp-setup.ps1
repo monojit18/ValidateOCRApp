208268c6939684e89cc00eb04f780bf0b6267db6
Monojit Datta <datta.monojit@gmail.com>
2020-12-30 17:51:50 +0530

    Update validateocrapp-setup.ps1

--- ValidateOCRApp-Setup/Setup/validateocrapp-setup.ps1
+++ ValidateOCRApp-Setup/Setup/validateocrapp-setup.ps1
@@ -1,11 +1,11 @@
 param([Parameter(Mandatory=$true)] [string] $resourceGroup = "<resource_group>",
       [Parameter(Mandatory=$true)] [string] $subscriptionId = "<subscription_id>",      
-      [Parameter(Mandatory=$true)] [string] $keyVaultName = "<keyvault_name>",
-      [Parameter(Mandatory=$true)] [string] $kvTemplateFileName = "keyvault-deploy",
+      [Parameter(Mandatory=$true)] [string] $keyVaultName = "<keyvault_name>",      
       [Parameter(Mandatory=$true)] [string] $vnetName = "srvless-workshop-vnet",
       [Parameter(Mandatory=$true)] [string] $vnetPrefix = "190.0.0.0/20",        
       [Parameter(Mandatory=$true)] [string] $subnetName = "srvless-workshop-subnet",
       [Parameter(Mandatory=$true)] [string] $subNetPrefix = "190.0.0.0/24",
+      [Parameter(Mandatory=$true)] [string] $kvTemplateFileName = "keyvault-deploy",
       [Parameter(Mandatory=$true)] [string] $networkTemplateFileName = "network-deploy",
       [Parameter(Mandatory=$true)] [string] $functionTemplateFileName = "validateocrapp-deploy",
       [Parameter(Mandatory=$true)] [string] $appName = "<app_name>",
@@ -17,7 +17,7 @@ $templatesFolderPath = $baseFolderPath + "/Templates"
 $keyvaultDeployCommand = "/KeyVault/$kvTemplateFileName.ps1 -rg $resourceGroup -fpath $templatesFolderPath -deployFileName $kvTemplateFileName -keyVaultName $keyVaultName -objectId $objectId"
 
 $networkNames = "-vnetName $vnetName -vnetPrefix $vnetPrefix -subnetName $subnetName -subNetPrefix $subNetPrefix"
-$networkDeployCommand = "Network/$networkTemplateFileName.ps1 -rg $resourceGroup -fpath $templatesFolderPath -deployFileName $networkTemplateFileName $networkNames"
+$networkDeployCommand = "/Network/$networkTemplateFileName.ps1 -rg $resourceGroup -fpath $templatesFolderPath -deployFileName $networkTemplateFileName $networkNames"
 
 $functionDeps = "-appName $appName -storageAccountName $storageAccountName -vnetName $vnetName"
 $functionDeployCommand = "/validateocrapp-deploy.ps1 -rg $resourceGroup -fpath $templatesFolderPath -deployFileName $functionTemplateFileName $functionDeps"
